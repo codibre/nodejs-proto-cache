06495a3fe0101627c3692f06819d0c56382d7b42
Thiago Santos <thiago.santos@maxmilhas.com.br>
2023-10-18 16:00:11 -0300

    refactor: accepting undefined result for StepTtl

--- src/types/cache-value.ts
+++ src/types/cache-value.ts
@@ -1,6 +1,6 @@
 import { Step } from './tree-type';
 
-export type StepTtl<T> = ((t: Step<T>) => number) | number;
+export type StepTtl<T> = ((t: Step<T>) => number | undefined) | number;
 
 export interface CacheSubItem {
 	key: string;
